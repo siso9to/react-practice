e77cbf228da4e5094af5b98546ab886e7c81c1f6
siso9to <yonepin@gmail.com>
2018-01-10 12:11:53 +0900

    modify setting state

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styles from "./css/app.css"
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.onClickButton = this.onClickButton.bind(this)
+  }
+
+
+  onClickButton() {
+    let { count } = this.state
+    this.setState({count: count + 1})
+  }
+
   render() {
-    return(
-      <div id="main">
-        <h1 className="title">{"Hello World"}</h1>
+    return (
+      <div>
+        <h1>{`クリックした回数: ${this.state.count}`}</h1>
+        <button onClick={this.onClickButton}>押す</button>
       </div>
     )
   }
